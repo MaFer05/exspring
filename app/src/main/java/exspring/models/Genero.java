5fb6a760e5df4390438c2be7c23851a18fd87be5
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2022-09-05 17:10:40 +0000

    exspring

---
+++ app/src/main/java/exspring/models/Genero.java
@@ -0,0 +1,33 @@
+package exspring.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue; 
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name="generos")
+public class Genero {
+    //chave primária = campo identificador
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id; 
+    private String nome;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
